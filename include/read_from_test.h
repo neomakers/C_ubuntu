e2a7ec7cfe4d9526cd25eae599d52a90064063bd
ufo001jone <ufo001jone@outlook.com>
2024-10-07 18:00:43 -0700

    packge read_test file

---
+++ include/read_from_test.h
@@ -0,0 +1,4 @@
+#ifndef _READ_FROM_TESH_H
+#define _READ_FROM_TESH_H
+int* read_array_from_file(const char *filename, int *n) ;
+#endif
--- src/main.c
+++ src/main.c
@@ -1,32 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
-
-// 封装函数，负责打开文件并读取数组
-int* read_array_from_file(const char *filename, int *n) {
-    FILE *file = fopen(filename, "r");
-    if (file == NULL) {
-        printf("无法打开文件 %s\n", filename);
-        return NULL;
-    }
-
-    // 读取数组长度
-    fscanf(file, "%d", n);
-    printf("数组长度: %d\n", *n);
-
-    // 分配内存存储数组
-    int *arr = (int *)malloc((*n) * sizeof(int));
-
-    // 读取数组元素
-    for (int i = 0; i < *n; i++) {
-        fscanf(file, "%d", &arr[i]);
-    }
-
-    // 关闭文件
-    fclose(file);
-    return arr;
-}
-
+#include "read_from_test.h"
 int main(int argc, char *argv[]) {
+	
     if (argc < 2) {
         printf("请提供测试数据文件名。\n");
         return 1;
